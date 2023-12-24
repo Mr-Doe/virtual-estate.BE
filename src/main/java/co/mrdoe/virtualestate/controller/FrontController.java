63734e4fe3619de6b485f1f3b8ecfa3ce240cd88
Mr.Doe <mr.doe.082@gmail.com>
2023-12-24 11:12:24 +0900

    init

--- src/main/java/co/mrdoe/virtualestate/controller/FrontController.java
+++ src/main/java/co/mrdoe/virtualestate/controller/FrontController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin")
-public class AdminController {
+@RequestMapping("/")
+public class FrontController {
 
 }
