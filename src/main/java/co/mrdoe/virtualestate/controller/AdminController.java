d0e5e477dd808b809d59859eafc6b0e3809987e8
Mr.Doe <mr.doe.082@gmail.com>
2023-12-27 17:52:26 +0900

    entity

--- src/main/java/co/mrdoe/virtualestate/controller/AdminController.java
+++ src/main/java/co/mrdoe/virtualestate/controller/AdminController.java
@@ -1,12 +1,12 @@
 package co.mrdoe.virtualestate.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
---
+++ src/main/java/co/mrdoe/virtualestate/entity/test.java
@@ -0,0 +1,17 @@
+package co.mrdoe.virtualestate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class test {
+
+    private int id;
+    private int link_id;
+    private String name;
+    private float z_level;
+    private String c_coords;
+    private String b_coords;
+    private int ln_code;
+
+}
